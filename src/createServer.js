df0f4764a04c5b49cbfd915f22fef3efb68c02b5
Polina Dabizha <polinkadabi29@gmail.com>
2025-04-12 12:20:48 +0300

    Help 2

--- src/createServer.js
+++ src/createServer.js
@@ -3,31 +3,48 @@
 
 const http = require('http');
 const fs = require('fs');
-// const url = require('url');
+const url = require('url');
 // const path = require('path');
 
 function createServer() {
   /* Write your code here */
   // Return instance of http.Server class
   const server = http.createServer((req, res) => {
-    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
-    const fileName =
-      normalizedURL.pathname.replace(/^\/file\//, '') || 'index.html';
+    const rawUrl = decodeURIComponent(req.url);
 
-    if (normalizedURL.pathname.includes('//')) {
+    console.log('Raw request URL:', rawUrl);
+
+    const passUrl = req.url;
+
+    console.log('Request URL:', passUrl);
+
+    if (passUrl.includes('//')) {
       res.statusCode = 404;
 
       return res.end();
     }
 
+    const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+
     if (normalizedURL.pathname.includes('..')) {
+      console.log('..');
       res.statusCode = 400;
       res.setHeader('Content-Type', 'text/plain');
 
-      return res.end('Invalid file path');
+      return res.end('No access');
     }
 
-    if (!normalizedURL.pathname.startsWith('/file/')) {
+    const fileName =
+      normalizedURL.pathname.replace(/^\/file\//, '') || 'index.html';
+
+    if (!passUrl.startsWith('/file/') && fs.existsSync(normalizedURL)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('No access');
+    }
+
+    if (!passUrl.startsWith('/file/')) {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
 
--- src/request.js
+++ src/request.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-console */
-const http = require('http');
+// const http = require('http');
 
-const options = {
-  hostname: 'localhost',
-  port: 5701,
-  path: '/index.html',
-};
+// const options = {
+//   hostname: 'localhost',
+//   port: 5701,
+//   path: '/file/../../index.html',
+// };
 
-http.get(options, (res) => {
-  res.setEncoding('utf8');
-  res.on('data', console.log);
-});
+// http.get(options, (res) => {
+//   res.setEncoding('utf8');
+//   res.on('data', console.log);
+// });
+
+const axios = require('axios');
+const BASE = 'http://localhost:5701';
+const href = BASE + '/file/../user/1/friends' + '?sex=m&age=25&age=35';
+
+console.log(href);
+axios.get(href).catch((err) => console.log(err));
